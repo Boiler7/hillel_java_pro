3e50385aedad13b524d00d99e18b31b089a2b12c
Kl <kir@>
2023-07-21 22:57:05 +0300

    HW3 upload

--- HW3/src/Animal.java
+++
@@ -1,9 +0,0 @@
-public abstract class Animal {
-    private String name;
-    public void run(int lenght){
-        System.out.println(name + " пробіг " + lenght + " м.");
-    }
-    public void swim(int lenght){
-        System.out.println(name + " проплив " + lenght + " м.");
-    }
-}
