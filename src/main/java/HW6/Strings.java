a022349c6321485e0ce9c663a517f97d50765b2a
Kl <kir@>
2023-08-04 19:06:07 +0300

    upload HW6

--- src/main/java/HW6/Strings.java
+++ src/main/java/HW6/Strings.java
@@ -3,7 +3,6 @@ package HW6;
 import java.util.Random;
 import java.util.Scanner;
 public class Strings {
-    private String guessedWord;
     public int findSymbolOccurrence(String text, String symbol){
         int before = text.length();
         String newText = text.replace(symbol, "");
@@ -37,24 +36,4 @@ public class Strings {
         }
         return reversedStr.toString().equalsIgnoreCase(text);
     }
-
-    public void gameGuess(){
-            String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
-                    "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", " pea", "peanut", "pear",
-                    "pepper", "pineapple", "pumpkin", "potato"};
-            var random = new Random();
-            var scanner = new Scanner(System.in);
-            String variants = "Variants: apple, orange, lemon, banana, apricot, avocado, broccoli, carrot, cherry, garlic, grape,\n " +
-                    "melon, leak, kiwi, mango, mushroom, nut, olive, pea, peanut, pear, pepper, pineapple, pumpkin, potato";
-        System.out.println("Try your variant. " + variants);
-            String gWord = scanner.nextLine();
-            guessedWord = words[random.nextInt(words.length)];
-            while(!gWord.equalsIgnoreCase(guessedWord)){
-                System.out.println("Try again\n" + guessedWord.charAt(0) +"" +  guessedWord.charAt(1) +"#############" + "\n" + variants);
-
-                gWord = scanner.nextLine();
-            }
-            System.out.println("You guessed!");
-        }
-
 }
