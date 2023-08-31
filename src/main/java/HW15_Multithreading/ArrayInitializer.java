3528d0a54969519d7deeb9f77bb0835479158a70
Kl <kir@>
2023-08-31 11:19:09 +0300

    HW 15-Multithreading

--- src/main/java/HW15_Multithreading/ArrayInitializer.java
+++ src/main/java/HW15_Multithreading/ArrayInitializer.java
@@ -6,18 +6,15 @@ public class ArrayInitializer {
     public static void init(double[] array){
         int n = array.length;
 
-        double[] a = Arrays.copyOfRange(array, 0, (n + 1)/2);
-        double[] b = Arrays.copyOfRange(array, (n + 1)/2, n);
-
         var thread1 = new Thread(() -> {
             for (int i = 0; i < n; i++) {
-                a[i] = (a[i] * Math.sin(0.2 + i / 5.0) * Math.cos(0.2 + i / 5.0) * Math.cos(0.4 + i / 2.0));
+                array[i] = (array[i] * Math.sin(0.2 + i / 5.0) * Math.cos(0.2 + i / 5.0) * Math.cos(0.4 + i / 2.0));
             }
         });
 
         var thread2 = new Thread(() -> {
             for (int i = 0; i < n; i++) {
-                b[i] = (b[i] * Math.sin(0.2 + i / 5.0) * Math.cos(0.2 + i / 5.0) * Math.cos(0.4 + i / 2.0));
+                array[i] = (array[i] * Math.sin(0.2 + i / 5.0) * Math.cos(0.2 + i / 5.0) * Math.cos(0.4 + i / 2.0));
             }
         });
 
@@ -29,9 +26,6 @@ public class ArrayInitializer {
             thread2.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }
-        for(int i = 0; i < b.length; i++) {
-            array[i] = b[n / 2 + i];
         }
     }
 }
