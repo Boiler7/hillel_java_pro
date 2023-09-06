884a6bd6023d371f1b8135a08383012cdea1cf7b
Kl <kir@>
2023-09-06 12:51:12 +0300

    HW16 with changes

--- src/main/java/HW16_Stream_API/Product.java
+++ src/main/java/HW16_Stream_API/Product.java
@@ -11,48 +11,43 @@ public class Product {
     private LocalDate date;
 
     public Product(){}
-    public Product(String category, double price, boolean isWithDiscount) {
+    public Product(String category, double price, boolean isWithDiscount, LocalDate date) {
         this.category = category;
         this.price = price;
         this.isWithDiscount = isWithDiscount;
-        this.date = LocalDate.now();
+        this.date = date;
     }
-    private final List<Product> productList = new ArrayList<>();
 
-    public void addProduct(Product product) {
-        productList.add(product);
-    }
-    public List<Product> getProductList(){
+
+    public List<Product> getProductList(List<Product> productList){
         return productList.stream()
                 .filter(product -> product.price >= 250)
                 .filter(product -> product.category.equalsIgnoreCase("Book"))
                 .toList();
     }
 
-    public List<Product> getDiscountedBooks(){
+    public List<Product> getDiscountedBooks(List<Product> productList){
         return productList.stream()
                 .filter(product -> product.category.equalsIgnoreCase("Book"))
-                .map(product -> {
-                            double priceWithDiscount = product.price - 0.1 * product.price;
-                            return new Product(product.category, priceWithDiscount, true);
-                })
+                .map(product -> new Product(product.category, product.price * 0.9, true, LocalDate.now()))
                 .collect(Collectors.toList());
     }
 
-    public Product findCheapestBook() {
+    public Product findCheapestBook(List<Product> productList) {
         return productList.stream()
                 .filter(product -> product.category.equalsIgnoreCase("Book"))
                 .min(Comparator.comparing(Product::getPrice))
                 .orElseThrow(() -> new RuntimeException("No cheapest book found"));
     }
 
-    public List<Product> getThreeLast(){
+    public List<Product> getThreeLast(List<Product> productList){
         return productList.stream()
-                .skip(productList.size() - 3)
+                .sorted(Comparator.comparing(Product::getDate).reversed())
+                .limit(3)
                 .collect(Collectors.toList());
     }
 
-    public double getCalculation(){
+    public double getCalculation(List<Product> productList){
         LocalDate currentDate = LocalDate.now();
         return productList.stream()
                 .filter(product -> product.getDate().getYear() == currentDate.getYear())
@@ -62,7 +57,7 @@ public class Product {
                 .sum();
     }
 
-    public Map<String, List<Product>> getGrouped(){
+    public Map<String, List<Product>> getGrouped(List<Product> productList){
         return productList.stream()
                 .collect(Collectors.groupingBy(Product::getCategory));
     }
@@ -74,7 +69,6 @@ public class Product {
         Product product = (Product) o;
         return Double.compare(product.price, price) == 0 && isWithDiscount == product.isWithDiscount && Objects.equals(category, product.category);
     }
-
 
     @Override
     public int hashCode() {
--- src/test/java/HW16_Stream_API/ProductTest.java
+++ src/test/java/HW16_Stream_API/ProductTest.java
@@ -2,6 +2,8 @@ package HW16_Stream_API;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,24 +15,27 @@ class ProductTest {
     @Test
     void getProductList() {
         var product = new Product();
-        product.addProduct(new Product("Book", 50, false));
-        product.addProduct(new Product("Book", 12, false));
-        product.addProduct(new Product("Book", 45, false));
-        product.addProduct(new Product("Book", 270, false));
-        product.addProduct(new Product("Book", 75, false));
-        product.addProduct(new Product("Book", 300, false));
-        product.addProduct(new Product("Book", 250, false));
-        product.addProduct(new Product("Tech", 90, false));
-        product.addProduct(new Product("Pack", 70, false));
-        product.addProduct(new Product("Toy", 30, false));
+
+        List<Product> productList = new ArrayList<>();
+
+        productList.add(new Product("Book", 50, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 12, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 45, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 270, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 75, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 300, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 250, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Tech", 90, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Pack", 70, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Toy", 30, false, LocalDate.of(2023, 5, 25)));
 
         var expectedList = List.of(
-                new Product("Book", 270, false),
-                new Product("Book", 300, false),
-                new Product("Book", 250, false)
+                new Product("Book", 270, false, LocalDate.of(2023, 5, 25)),
+                new Product("Book", 300, false, LocalDate.of(2023, 5, 25)),
+                new Product("Book", 250, false, LocalDate.of(2023, 5, 25))
         );
 
-        assertEquals(expectedList, product.getProductList());
+        assertEquals(expectedList, product.getProductList(productList));
 
     }
 
@@ -38,108 +43,121 @@ class ProductTest {
     void getDiscountedBooks() {
         var product = new Product();
 
-        product.addProduct(new Product("Book", 50, false));
-        product.addProduct(new Product("Book", 45, false));
-        product.addProduct(new Product("Book", 270, false));
-        product.addProduct(new Product("Pack", 70, false));
-        product.addProduct(new Product("Toy", 30, false));
+        List<Product> productList = new ArrayList<>();
+
+        productList.add(new Product("Book", 50, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 45, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 270, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Pack", 70, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Toy", 30, false , LocalDate.of(2023, 5, 25)));
 
         var expectedList = List.of(
-                new Product("Book", 45, true),
-                new Product("Book", 40.5, true),
-                new Product("Book", 243.0, true)
+                new Product("Book", 45, true, LocalDate.of(2023, 5, 25)),
+                new Product("Book", 40.5, true, LocalDate.of(2023, 5, 25)),
+                new Product("Book", 243.0, true, LocalDate.of(2023, 5, 25))
         );
 
-        assertEquals(expectedList, product.getDiscountedBooks());
+        assertEquals(expectedList, product.getDiscountedBooks(productList));
     }
 
     @Test
     void findCheapestBook() {
         var product = new Product();
-        product.addProduct(new Product("Book", 50, false));
-        product.addProduct(new Product("Book", 12, false));
-        product.addProduct(new Product("Book", 45, false));
-        product.addProduct(new Product("Book", 270, false));
-        product.addProduct(new Product("Book", 75, false));
-        product.addProduct(new Product("Book", 300, false));
-        product.addProduct(new Product("Book", 250, false));
-        product.addProduct(new Product("Tech", 90, false));
-        product.addProduct(new Product("Pack", 70, false));
-        product.addProduct(new Product("Toy", 30, false));
 
-        var res = new Product("Book", 12, false);
+        List<Product> productList = new ArrayList<>();
 
-        assertEquals(res, product.findCheapestBook());
+        productList.add(new Product("Book", 50, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 12, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 45, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 270, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 75, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 300, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 250, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Tech", 90, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Pack", 70, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Toy", 30, false, LocalDate.of(2023, 5, 25)));
+
+        var res = new Product("Book", 12, false, LocalDate.of(2023, 5, 25));
+
+        assertEquals(res, product.findCheapestBook(productList));
     }
 
     @Test
     void getThreeLast() {
         var product = new Product();
-        product.addProduct(new Product("Book", 50, false));
-        product.addProduct(new Product("Book", 12, false));
-        product.addProduct(new Product("Book", 45, false));
-        product.addProduct(new Product("Book", 270, false));
-        product.addProduct(new Product("Book", 75, false));
-        product.addProduct(new Product("Book", 300, false));
-        product.addProduct(new Product("Book", 250, false));
-        product.addProduct(new Product("Tech", 90, false));
-        product.addProduct(new Product("Pack", 70, false));
-        product.addProduct(new Product("Toy", 30, false));
+
+        List<Product> productList = new ArrayList<>();
+
+        productList.add(new Product("Book", 50, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 12, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 45, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 270, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 75, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 300, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 250, false, LocalDate.of(2023, 8, 25)));
+        productList.add(new Product("Tech", 90, false, LocalDate.of(2023, 7, 25)));
+        productList.add(new Product("Pack", 70, false, LocalDate.of(2023, 6, 25)));
+        productList.add(new Product("Toy", 30, false, LocalDate.of(2023, 5, 25)));
 
         var expectedList = List.of(
-            new Product("Tech", 90, false),
-            new Product("Pack", 70, false),
-            new Product("Toy", 30, false)
+                new Product("Book", 250, false, LocalDate.of(2023, 8, 25)),
+                new Product("Tech", 90, false, LocalDate.of(2023, 6, 25)),
+                new Product("Pack", 70, false, LocalDate.of(2023, 7, 25))
         );
 
-        assertEquals(expectedList, product.getThreeLast());
+        assertEquals(expectedList, product.getThreeLast(productList));
     }
 
     @Test
     void getCalculation() {
         var product = new Product();
-        product.addProduct(new Product("Book", 50, false));
-        product.addProduct(new Product("Book", 12, false));
-        product.addProduct(new Product("Book", 45, false));
-        product.addProduct(new Product("Book", 270, false));
-        product.addProduct(new Product("Book", 75, false));
-        product.addProduct(new Product("Book", 300, false));
-        product.addProduct(new Product("Book", 250, false));
-        product.addProduct(new Product("Tech", 90, false));
-        product.addProduct(new Product("Pack", 70, false));
-        product.addProduct(new Product("Toy", 30, false));
+
+        List<Product> productList = new ArrayList<>();
+
+        productList.add(new Product("Book", 50, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 12, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 45, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 270, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 75, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 300, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 250, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Tech", 90, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Pack", 70, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Toy", 30, false, LocalDate.of(2023, 5, 25)));
 
         var expectedList = 182;
 
-        assertEquals(expectedList, product.getCalculation());
+        assertEquals(expectedList, product.getCalculation(productList));
     }
 
     @Test
     void getGrouped() {
         var product = new Product();
-        product.addProduct(new Product("Book", 50, false));
-        product.addProduct(new Product("Book", 12, false));
-        product.addProduct(new Product("Book", 45, false));
-        product.addProduct(new Product("Tech", 90, false));
-        product.addProduct(new Product("Pack", 70, false));
-        product.addProduct(new Product("Toy", 30, false));
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product("Book", 50, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 12, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Book", 45, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Tech", 90, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Pack", 70, false, LocalDate.of(2023, 5, 25)));
+        productList.add(new Product("Toy", 30, false, LocalDate.of(2023, 5, 25)));
 
         var expectedMap = Map.of(
                 "Book", List.of(
-                        new Product("Book", 50, false),
-                        new Product("Book", 12, false),
-                        new Product("Book", 45, false)
+                        new Product("Book", 50, false, LocalDate.of(2023, 5, 25)),
+                        new Product("Book", 12, false, LocalDate.of(2023, 5, 25)),
+                        new Product("Book", 45, false, LocalDate.of(2023, 5, 25))
                 ),
                 "Tech", List.of(
-                        new Product("Tech", 90, false)
+                        new Product("Tech", 90, false, LocalDate.of(2023, 5, 25))
                 ),
                 "Pack", List.of(
-                        new Product("Pack", 70, false)
+                        new Product("Pack", 70, false, LocalDate.of(2023, 5, 25))
                 ),
-                "Toy", List.of(new Product("Toy", 30, false)
+                "Toy", List.of(new Product("Toy", 30, false, LocalDate.of(2023, 5, 25))
                 )
         );
 
-        assertEquals(expectedMap, product.getGrouped());
+        assertEquals(expectedMap, product.getGrouped(productList));
     }
 }
