5b0cf9fdcbcf678a6498cd3f93a323a6ec9cc241
Kl <kir@>
2023-10-29 00:44:03 +0300

    HW26

--- src/main/java/HW26/HeroClient.java
+++ src/main/java/HW26/HeroClient.java
@@ -13,22 +13,25 @@ import java.util.Scanner;
 
 public class HeroClient {
     private static ServerSocket server;
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        try (Socket socket = new Socket(HeroProtocol.SERVER_HOST, HeroProtocol.SERVER_PORT)) {
+    public static void main(String[] args)  {
+        try (Socket socket = new Socket(HeroServer.SERVER_HOST, HeroServer.SERVER_PORT)) {
             Scanner scanner = new Scanner(System.in);
             var out = new PrintWriter(socket.getOutputStream(), true);
             var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             while (true) {
-                String message = scanner.next();
+                String message = scanner.nextLine();
                 out.println(message);
+
                 if(message.equals("-exit")){
-                    System.out.println("Shutting down the server");
+                    System.out.println("Shutting down the connection...");
                     in.close();
                     out.close();
+                    socket.close();
                     break;
                 }
+                String messageFromServer = in.readLine();
+                System.out.println("Server: " + (messageFromServer));
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/HW26/HeroServer.java
+++ src/main/java/HW26/HeroServer.java
@@ -5,59 +5,43 @@ import HW25_Patterns_Integration_Testing.HeroMovieService;
 import HW25_Patterns_Integration_Testing.HeroService;
 import HW7_Exceptions.ArraySizeException;
 import org.postgresql.ds.PGSimpleDataSource;
+
 import javax.sql.DataSource;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HeroServer {
-    private final static int SERVER_PORT = 8080;
+    public final static String SERVER_HOST = "localhost";
+    public final static int SERVER_PORT = 8080;
     private static ServerSocket server;
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, IOException {
+    public static void main(String[] args) throws IOException {
         server = new ServerSocket(SERVER_PORT);
 
-        var socket = server.accept();
-        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        var out = new PrintWriter(socket.getOutputStream(), true);
-
         while (true) {
-
             System.out.println("Waiting for the client request");
 
-            String message = in.readLine();
+            Socket socket = server.accept();
 
+            var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//            var out = new PrintWriter(socket.getOutputStream(), true);
 
-            if (message.equalsIgnoreCase("-exit")) {
-                System.out.println("Shutting down the server");
-                in.close();
-                out.close();
-                socket.close();
-                server.close();
+            String message;
+
+            message = in.readLine();
+            if (message.equals("-exit"))
                 break;
-            } else if (message.startsWith("-name")) {
-                System.out.println("Finding request");
-                message = message.substring(5);
-                out.println("ldkjfs" + message);
 
-                HeroService heroService = new HeroService(new HeroDaoImplementation(dataSource()), new HeroMovieService());
-                var heroDao = new HeroDaoImplementation(dataSource()).findByName(message);
-
-                if (heroDao == null) {
-                    out.println("Hero is not found");
-                } else {
-
+            Thread thread = new Thread(() -> {
+                try {
+                    HeroProtocol.run(socket);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-            }
+            });
+            thread.start();
         }
     }
 
-    private static DataSource dataSource() {
-        var ds = new PGSimpleDataSource();
-        ds.setServerNames(new String[]{"localhost"});
-        ds.setDatabaseName("postgres");
-        ds.setUser("hillel");
-        ds.setPassword("hillel");
-        return ds;
-    }
 }
