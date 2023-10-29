b4e4287ee8cd05b10bb2b8495e746851150f5370
Kl <kir@>
2023-10-29 15:35:52 +0200

    HW26

---
+++ src/main/java/HW26/HeroClient.java
@@ -0,0 +1,39 @@
+package HW26;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class HeroClient {
+    private static ServerSocket server;
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(HeroServer.SERVER_HOST, HeroServer.SERVER_PORT)) {
+            Scanner scanner = new Scanner(System.in);
+            var out = new PrintWriter(socket.getOutputStream(), true);
+            var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            while (true) {
+                System.out.println("Enter a command: ");
+                String message = scanner.nextLine();
+                out.println(message);
+
+                if (message.equals("-exit")) {
+                    System.out.println("Shutting down the connection...");
+                    in.close();
+                    out.close();
+                    socket.close();
+                    break;
+                }
+                String messageFromServer = in.readLine();
+                System.out.println("Server: " + (messageFromServer));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/HW26/HeroProtocol.java
@@ -0,0 +1,67 @@
+package HW26;
+
+import HW24_JDBC.HeroDaoImplementation;
+import HW25_Patterns_Integration_Testing.HeroMovieService;
+import HW25_Patterns_Integration_Testing.HeroService;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+public class HeroProtocol {
+    public static void run(Socket socket) throws IOException {
+        while (true) {
+            var out = new PrintWriter(socket.getOutputStream(), true);
+            var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String message = in.readLine();
+
+            if (message.equals("-exit")) {
+                System.out.println("Client disconnected");
+                in.close();
+                socket.close();
+                out.close();
+                break;
+            }
+
+            if (message.startsWith("-name")) {
+                System.out.println("Finding request");
+                String heroName = message.substring(6);
+
+                HeroService heroService = new HeroService(new HeroDaoImplementation(dataSource()), new HeroMovieService());
+                var heroDao = new HeroDaoImplementation(dataSource());
+                var hero = heroDao.findByName(heroName).get(0);
+                System.out.println("Hero's name: " + hero.getName());
+
+                var herodto = heroService.map(hero);
+                System.out.println(herodto);
+                if (herodto.getMovies() == null & herodto.getName() == null) {
+                    out.println("Hero is not found");
+                } else {
+                    System.out.println("Send hero");
+                    out.println("Name:" +herodto.getName() + "\tMoveies: " + herodto.getMovies());
+                }
+//                out.println(hero);
+//                System.out.println(heroDao.findByName(heroName));
+//                out.println(heroDao.findByName(heroName));
+//                out.println(heroDao.findByName("Abe Sapien"));
+            }
+        }
+    }
+
+    private static DataSource dataSource() {
+        var ds = new PGSimpleDataSource();
+        ds.setServerNames(new String[]{"localhost"});
+        ds.setDatabaseName("postgres");
+        ds.setUser("hillel");
+        ds.setPassword("hillel");
+        return ds;
+    }
+}
+
+
