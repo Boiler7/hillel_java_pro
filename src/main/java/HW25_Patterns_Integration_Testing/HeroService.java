7d14abd47c8f8fd785e2edc0b4a09d7bda5f815d
Kl <kir@>
2023-11-07 22:28:19 +0200

    HW28

--- src/main/java/HW25_Patterns_Integration_Testing/HeroService.java
+++ src/main/java/HW25_Patterns_Integration_Testing/HeroService.java
@@ -2,11 +2,14 @@ package HW25_Patterns_Integration_Testing;
 
 import HW24_JDBC.Hero;
 import HW24_JDBC.HeroDao;
+import HW24_JDBC.HeroDaoImplementation;
+import HW28_REST.HeroCreationRequest;
+import org.postgresql.ds.PGSimpleDataSource;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 public class HeroService {
-
     private final HeroDao heroDao;
     private final HeroMovieService heroMovieService;
 
@@ -15,18 +18,31 @@ public class HeroService {
         this.heroMovieService = heroMovieService;
     }
 
-    public HeroDto updateHero(HeroDto heroDto, long id) {
-        heroDao.update(Hero.builder()
-                        .name(heroDto.getName())
-                        .id(id)
-                        .build());
-        return getHeroById(id);
-    }
-
     public List<HeroDto> getHeroes() {
         return heroDao.findAll().stream()
-                .map(hero -> map(hero))
+                .map(this::map)
                 .toList();
+    }
+
+    public HeroDto getHeroById(long id) {
+        return map(heroDao.findById(id).get(0));
+    }
+
+
+    public void create(HeroCreationRequest request) {
+        var heroDaoImp = new HeroDaoImplementation(dataSource());
+        var hero = new Hero(null, request.name(), request.gender(), request.eyeColor(), request.race(), request.hairColor(),
+                request.height(), request.publisher(), request.skinColor(), request.alignment(), request.weigh());
+        heroDaoImp.create(hero);
+    }
+
+
+    public HeroDto updateHero(long id, HeroDto heroDto) {
+        heroDao.update(Hero.builder()
+                .name(heroDto.getName())
+                .id(id)
+                .build());
+        return getHeroById(id);
     }
 
     public HeroDto map(Hero hero) {
@@ -34,5 +50,18 @@ public class HeroService {
                 .name(hero.getName())
                 .movies(heroMovieService.getPlayedIn(hero.getName()))
                 .build();
+    }
+
+    public boolean delete(long id){
+        return heroDao.delete(id);
+    }
+
+    private static DataSource dataSource() {
+        var ds = new PGSimpleDataSource();
+//        ds.setServerNames(new String[]{"localhost"});
+        ds.setDatabaseName("postgres");
+        ds.setUser("hillel");
+        ds.setPassword("hillel");
+        return ds;
     }
 }
