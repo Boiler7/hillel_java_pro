23425b795de69e3032c917912683ac448a783471
Boiler7 <77234253+Boiler7@users.noreply.github.com>
2023-11-10 15:19:01 +0200

    Spring Core (#63)
    * Spring Core
    
    * Spring Core
    
    ---------
    
    Co-authored-by: Kl <kir@>

---
+++ src/main/java/hw29/spring/core/HeroMain.java
@@ -0,0 +1,15 @@
+package hw29.spring.core;
+
+import HW25_Patterns_Integration_Testing.HeroService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class HeroMain {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(HeroConfiguration.class);
+        var repository = context.getBean(HeroService.class);
+
+        var hero = repository.getHeroById(5);
+
+        System.out.println("Name: " + hero.getName() + "\nMovies: " + hero.getMovies());
+    }
+}
