800c9a8fb7445be58ea4d3a2d6236d7df1659719
Kl <kir@>
2023-11-28 19:18:18 +0200

    db versioning and bank

--- src/main/java/bank/account/AccountController.java
+++ src/main/java/bank/account/AccountController.java
@@ -1,7 +1,6 @@
 package bank.account;
 
-import bank.person.PersonDto;
-import bank.person.PersonService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
@@ -11,33 +10,36 @@ import java.util.Optional;
 
 
 @RestController
-@RequestMapping("/api/accounts/")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
 
-    @GetMapping()
+    @GetMapping("/accounts/")
     public List<AccountDto> accounts(Pageable pageable) {
         return accountService.getAccounts(pageable);
     }
-    @GetMapping("/{id}")
+
+    @GetMapping("/accounts/{id}")
     public Optional<AccountDto> account(@PathVariable("id") Long id){
         return accountService.getAccount(id);
     }
-    @PostMapping("create")
+
+    @PostMapping("/accounts")
     public AccountDto createAccount(@RequestBody AccountDto request) {
         return accountService.create(request);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/accounts/{id}")
     public AccountDto updateAccount(
             @PathVariable("id") Long id,
             @RequestBody AccountDto request) {
         return accountService.update(id, request);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/accounts/{id}")
     public void deleteAccount(@PathVariable("id") String uid) {
         accountService.delete(uid);
     }
+
 }
--- src/main/java/bank/person/PersonController.java
+++ src/main/java/bank/person/PersonController.java
@@ -2,33 +2,32 @@ package bank.person;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/persons")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class PersonController {
     private final PersonService personService;
 
-    @GetMapping
+    @GetMapping("/persons")
     public List<PersonDto> getAll(Pageable pageable) {
         return personService.getPersons(pageable);
     }
 
-    @GetMapping("/{uid}")
+    @GetMapping("/persons/{uid}")
     public PersonDto person(@PathVariable("uid") String uid) {
         return personService.getPerson(uid);
     }
 
-    @PostMapping
+    @PostMapping("/persons")
     public PersonDto createPerson(@RequestBody PersonDto request) {
         return personService.create(request);
     }
 
-    @PutMapping("/{uid}")
+    @PutMapping("/persons/{uid}")
     public PersonDto updatedPerson(
             @PathVariable("uid") String uid,
             @RequestBody PersonDto request) {
@@ -36,7 +35,7 @@ public class PersonController {
         return personService.update(uid, request);
     }
 
-    @DeleteMapping("/{uid}")
+    @DeleteMapping("/persons/{uid}")
     public void deletePerson(@PathVariable("uid") String uid) {
         personService.delete(uid);
     }
