6e25791625f95845470750d85898e7b61845bfbf
Kl <kir@>
2023-12-02 22:04:25 +0200

    db versioning and bank

---
+++ src/test/java/bank/WebIntegrationTest.java
@@ -0,0 +1,24 @@
+package bank;
+
+import bank.account.AccountRepository;
+import bank.person.PersonRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public abstract class WebIntegrationTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected PersonRepository personRepository;
+    @Autowired
+    protected AccountRepository accountRepository;
+    @Autowired
+    protected ObjectMapper objectMapper;
+}
