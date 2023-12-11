9d3a24b32ef18f6521f6463007e5d30a2af1db80
Kl <kir@>
2023-12-11 16:24:07 +0200

    WireMock test

--- src/test/java/bank/WebIntegrationTest.java
+++ src/test/java/bank/WebIntegrationTest.java
@@ -1,12 +1,17 @@
 package bank;
 
+
 import bank.account.AccountRepository;
 import bank.person.PersonRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -21,4 +26,17 @@ public abstract class WebIntegrationTest {
     protected AccountRepository accountRepository;
     @Autowired
     protected ObjectMapper objectMapper;
+    @Autowired
+    protected WireMockServer wireMockServer;
+
+//    @AfterAll
+//    void tearDown() {
+//        wireMockServer.resetAll();
+//    }
+
+    @DynamicPropertySource
+    public static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("wiremock.baseurl", WireMockConfig.wireMockServer::baseUrl);
+    }
+
 }
---
+++ src/test/java/bank/api/converter/DummyControllerIntegrationTest.java
@@ -0,0 +1,48 @@
+package bank.api.converter;
+
+import bank.WebIntegrationTest;
+import bank.api.converter.model.ConverterResponse;
+import bank.api.converter.model.ConverterResponseData;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class DummyControllerIntegrationTest extends WebIntegrationTest {
+    @Test
+    public void shouldConvert() throws Exception {
+        var response = ConverterResponse.builder()
+                .data(Map.of("UAH", ConverterResponseData.builder()
+                        .code("UAH")
+                        .value(39.1425)
+                        .build()))
+                .build();
+
+        wireMockServer.stubFor(WireMock.get(urlEqualTo("/api/converter"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(objectMapper.writeValueAsString(response))));
+
+        var amount = mockMvc.perform(get("/api/converter")
+                        .param("from", "USD")
+                        .param("to", "UAH")
+                        .param("amount", "100")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        double score = Double.parseDouble(amount);
+
+        assertThat(score, equalTo(3914.25));
+    }
+}
