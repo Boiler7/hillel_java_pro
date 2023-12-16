03df87650e94278f9a8123782a8c147e38eb1ac4
Kl <kir@>
2023-12-16 15:42:12 +0200

    Wiremock

--- src/test/java/bank/api/converter/ConverterControllerIntegrationTest.java
+++ src/test/java/bank/api/converter/ConverterControllerIntegrationTest.java
@@ -1,22 +1,46 @@
 package bank.api.converter;
 
 import bank.WebIntegrationTest;
+import bank.api.converter.model.ConverterResponse;
+import bank.api.converter.model.ConverterResponseData;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+
+import java.util.Map;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class DummyControllerIntegrationTest extends WebIntegrationTest {
+@Slf4j
+public class ConverterControllerIntegrationTest extends WebIntegrationTest {
     @Test
     public void shouldConvert() throws Exception {
-        stubFor(WireMock.get(urlEqualTo("http://localhost:8080/api/converter"))
+//        WebClient.builder()
+//                .baseUrl("http://localhost:8080")
+//                .defaultHeaders(header -> header.setBasicAuth("username", "password"))
+//                .build()
+//                .get()
+//                .uri("/api/converter")
+//                .retrieve()
+//                .toEntity(String.class)
+//                .block();
+
+        var response = ConverterResponse.builder()
+                .data(Map.of("USD", ConverterResponseData.builder()
+                        .code("USD")
+                        .value(39.1425)
+                        .build()))
+                .build();
+
+        wireMockServer.stubFor(WireMock.get(urlEqualTo("/api/converter"))
                 .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")));
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(objectMapper.writeValueAsString(response))));
 
         var amount = mockMvc.perform(get("/api/converter")
                         .param("from", "UAH")
