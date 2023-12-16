e74d183ab0fe2f389f57f0c668a3a3cf14936acb
Kl <kir@>
2023-12-16 15:50:55 +0200

    Log

--- src/main/java/bank/api/converter/PersonOperationsService.java
+++ src/main/java/bank/api/converter/PersonOperationsService.java
@@ -1,6 +1,7 @@
 package bank.api.converter;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Currency;
@@ -8,13 +9,15 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @AllArgsConstructor
-
+@Slf4j
 @Service
 public class PersonOperationsService {
     private final CurrencyConverter currencyConverter;
     public double convert(Currency from, Currency to, double amount) throws ExecutionException, InterruptedException {
         CompletableFuture<Double> collable
                 =CompletableFuture.supplyAsync(() -> currencyConverter.convert(from, to, amount));
+
+        log.info("Convert: return={}", collable.get());
 
         return collable.get();
     }
