2bd41b54ab4169c9787875cd59f602cc06816606
Kl <kir@>
2023-12-16 17:32:30 +0200

    Spring Data JPA

---
+++ src/main/java/hw32/spring/data/jpa/user/User.java
@@ -0,0 +1,23 @@
+package hw32.spring.data.jpa.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uid;
+    private String name;
+    private String email;
+    private UserRole role;
+}
