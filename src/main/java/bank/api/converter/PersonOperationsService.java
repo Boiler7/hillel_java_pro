1ffbe704aca81f578467b0d133627735f464c796
Boiler7 <77234253+Boiler7@users.noreply.github.com>
2023-12-18 13:59:31 +0200

    Log and Multithreading (#74)
    * Log and Multithreading
    
    * Log
    
    * Log
    
    ---------
    
    Co-authored-by: Kl <kir@>

---
+++ src/main/java/bank/api/converter/PersonOperationsService.java
@@ -0,0 +1,24 @@
+package bank.api.converter;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Currency;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@AllArgsConstructor
+@Slf4j
+@Service
+public class PersonOperationsService {
+    private final CurrencyConverter currencyConverter;
+    public double convert(Currency from, Currency to, double amount) throws ExecutionException, InterruptedException {
+        CompletableFuture<Double> callable
+                =CompletableFuture.supplyAsync(() -> currencyConverter.convert(from, to, amount));
+
+        log.info("Convert: return={}", callable.get());
+
+        return callable.get();
+    }
+}
