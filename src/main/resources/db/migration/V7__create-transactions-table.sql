5ac4d0804c24541cba517b74752d6d797a0513f9
Kl <kir@>
2023-12-23 20:57:46 +0200

    creating transactions

---
+++ src/main/resources/db/migration/V7__create-transactions-table.sql
@@ -0,0 +1,16 @@
+create table if not exists transactions
+(
+    id               serial constraint card_pk primary key,
+    uid              varchar,
+    from_card_id     varchar,
+    from_account_id  INTEGER,
+    to_card_id       varchar,
+    to_account_id    varchar,
+    amount           INTEGER,
+    created_at       timestamp not null,
+    updated_at       timestamp not null,
+    foreign key (from_card_id) references cards(id),
+    foreign key (from_account_id) references accounts(id),
+    foreign key (to_card_id) references cards(id),
+    foreign key (to_account_id) references accounts(id)
+);
